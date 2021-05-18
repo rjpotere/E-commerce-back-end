21ad97547a3c813021c4b471f4a1671dcea18a2c
Potears <ryan.potere@gmail.com>
2021-05-17 22:47:04 -0400

    GET, POST and PUT working properly in insomnia.

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,16 +5,33 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-products.belongsTo(category, {
+Product.belongsTo(Category, {
   foreignKey: 'category_id'
 });
 
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'Product_tag'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'Tag'
+});
 
 module.exports = {
   Product,
